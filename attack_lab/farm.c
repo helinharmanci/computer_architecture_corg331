c82c29c01e36dba9251c33911bbbbeee093a7f1c
Helin Harmancı <64043277+helinharmanci@users.noreply.github.com>
2023-05-07 03:34:59 +0300

    Add files via upload

---
+++ attack_lab/farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_491(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+unsigned getval_299()
+{
+    return 3351380296U;
+}
+
+void setval_197(unsigned *p)
+{
+    *p = 3287976264U;
+}
+
+unsigned addval_496(unsigned x)
+{
+    return x + 3284271176U;
+}
+
+unsigned getval_213()
+{
+    return 3285616968U;
+}
+
+unsigned addval_399(unsigned x)
+{
+    return x + 2499447112U;
+}
+
+void setval_407(unsigned *p)
+{
+    *p = 2445773128U;
+}
+
+unsigned getval_478()
+{
+    return 3284633928U;
+}
+
+unsigned addval_446(unsigned x)
+{
+    return x + 2462550344U;
+}
+
+unsigned getval_291()
+{
+    return 3284271432U;
+}
+
+void setval_259(unsigned *p)
+{
+    *p = 1479218576U;
+}
+
+void setval_404(unsigned *p)
+{
+    *p = 3285616969U;
+}
+
+void setval_462(unsigned *p)
+{
+    *p = 3284271432U;
+}
+
+unsigned getval_232()
+{
+    return 3254421832U;
+}
+
+unsigned getval_240()
+{
+    return 3599321270U;
+}
+
+void setval_485(unsigned *p)
+{
+    *p = 2429979080U;
+}
+
+unsigned getval_252()
+{
+    return 1481981731U;
+}
+
+unsigned getval_451()
+{
+    return 2425393368U;
+}
+
+void setval_388(unsigned *p)
+{
+    *p = 3281096792U;
+}
+
+unsigned getval_278()
+{
+    return 4203301043U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
